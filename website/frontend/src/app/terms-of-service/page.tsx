d7b59c2f8463fd4ee789323a1b18151fc2c1d8e5
Mensun <mensun@bu.edu>
2025-01-19 18:32:42 -0500

    added template TOS page

---
+++ website/frontend/src/app/terms-of-service/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Headline from "@/components/pages/privacy-policy/Headline";
+
+const TermsOfServicePage: React.FC = () => {
+
+
+  return (
+    <>
+      <Headline />
+
+    </>
+  )
+}
+
+export default TermsOfServicePage
